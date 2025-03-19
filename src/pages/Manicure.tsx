833d0693e46030bb9659f333e50af98ff61a73c2
Henry <89488963+Old-Lai@users.noreply.github.com>
2025-03-19 15:38:29 -0700

    converted price cards to generate dynamically

--- src/pages/Manicure.tsx
+++ src/pages/Manicure.tsx
@@ -16,41 +16,79 @@ import {
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import Autoplay from "embla-carousel-autoplay";
 import { img_manager } from "@/assets";
+import { LanguageServices } from "@/lib/interfaces";
 
+enum Current_Lang {
+  English = "en",
+  Chinese = "zh",
+}
+
+const current_lang = Current_Lang.English;
 const nails_imgs = img_manager.get.nails;
-
-const nails_service_list = {
-  nail_services: [
-    { name: "Regular Gel", price: 35 },
-    { name: "Building Gel", price: 60 },
-    { name: "Gel Pedicure", price: 60 },
-    { name: "Extension", price: 80 },
-    { name: "Removal", price: 15 },
-    { name: "Hand Spa (anion capsule)", price: 38 },
-    { name: "Germany LCN Foot Spa", price: 65 },
-  ],
-  nail_design: [
-    { name: "Cat Eye", price: 20 },
-    { name: "Chrome", price: 20 },
-    { name: "French", price: 20 },
-    { name: "Hand Painting", price: 30 },
-    { name: "Ombré", price: 20 },
-    { name: "Blooming", price: 30 },
-  ],
-  others: [
-    { name: "Tip", price: "8 /per" },
-    { name: "Diamond", price: "5-10 /per" },
-    { name: "Applique", price: 5 },
-  ],
+const nails_service_list: LanguageServices = {
+  en: {
+    "Nail Services": [
+      { name: "Regular Gel", price: 35 },
+      { name: "Building Gel", price: 60 },
+      { name: "Gel Pedicure", price: 60 },
+      { name: "Extension", price: 80 },
+      { name: "Removal", price: 15 },
+      { name: "Hand Spa (anion capsule)", price: 38 },
+      { name: "Germany LCN Foot Spa", price: 65 },
+    ],
+    "Nail Design": [
+      { name: "Cat Eye", price: 20 },
+      { name: "Chrome", price: 20 },
+      { name: "French", price: 20 },
+      { name: "Hand Painting", price: 30 },
+      { name: "Ombré", price: 20 },
+      { name: "Blooming", price: 30 },
+    ],
+    "Other Services": [
+      { name: "Tip", price: "8 /per" },
+      { name: "Diamond", price: "5-10 /per" },
+      { name: "Applique", price: 5 },
+    ],
+  },
+  zh: {
+    nail_services: [
+      { name: "普通胶油", price: 35 },
+      { name: "建构（单色）", price: 60 },
+      { name: "足部-单色Gels（包含普通修脚）", price: 60 },
+      { name: "甲片延长", price: 80 },
+      { name: "卸甲", price: 15 },
+      { name: "太空舱手部护理", price: 38 },
+      { name: "德国贵妇LCN足部护理", price: 65 },
+    ],
+    nail_design: [
+      { name: "猫眼", price: 20 },
+      { name: "魔法粉", price: 20 },
+      { name: "法式", price: 20 },
+      { name: "手绘", price: 30 },
+      { name: "渐变", price: 20 },
+      { name: "晕染", price: 30 },
+    ],
+    others: [
+      { name: "单个甲片", price: "8 /per" },
+      { name: "单颗钻石", price: "5-10 /per" },
+      { name: "饰品", price: 5 },
+    ],
+  },
 };
+
+const className = {
+  card: "md:min-w-[30rem]",
+  card_table: "md:text-lg",
+};
+const styles = className;
 
 export default function Manicure() {
   return (
     <div className="flex w-full flex-col items-center bg-gradient-to-t from-(--color-secondary) to-white md:p-15">
-      <section>
+      <section className="mb-5">
         <Carousel
-          className="mx-2 my-5 w-full md:m-0 md:max-w-5xl"
-          plugins={[Autoplay({ delay: 2000 })]}
+          className="mx-2 my-5 w-full md:m-0 md:max-w-7xl"
+          plugins={[Autoplay({ delay: 3000 })]}
         >
           <CarouselContent className="flex items-center">
             {Object.keys(nails_imgs).map((imgObj) => (
@@ -78,70 +116,32 @@ export default function Manicure() {
           />
         </Carousel>
       </section>
-      <section className="mb-5 w-full space-y-5 px-5 md:max-w-[35rem]">
-        <Card>
-          <CardContent>
-            <CardTitle>Nail Services</CardTitle>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Sevice</TableHead>
-                  <TableHead className="text-right">Price</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {nails_service_list.nail_services.map((service) => (
-                  <TableRow key={service.name}>
-                    <TableCell>{service.name}</TableCell>
-                    <TableCell className="text-right font-semibold">{`$${service.price}`}</TableCell>
+      <section className="mb-5 w-full space-y-5 px-5 md:mb-0 md:max-w-[40rem] md:text-2xl">
+        {Object.keys(nails_service_list[current_lang]).map((service_key) => (
+          <Card className={styles.card} key={service_key}>
+            <CardContent>
+              <CardTitle>{service_key}</CardTitle>
+              <Table className={styles.card_table}>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Sevice</TableHead>
+                    <TableHead className="text-right">Price</TableHead>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <CardTitle>Nail Design</CardTitle>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Sevice</TableHead>
-                  <TableHead className="text-right">Price</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {nails_service_list.nail_design.map((service) => (
-                  <TableRow key={service.name}>
-                    <TableCell>{service.name}</TableCell>
-                    <TableCell className="text-right font-semibold">{`$${service.price}`}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <CardTitle>Others</CardTitle>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Sevice</TableHead>
-                  <TableHead className="text-right">Price</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {nails_service_list.others.map((service) => (
-                  <TableRow key={service.name}>
-                    <TableCell>{service.name}</TableCell>
-                    <TableCell className="text-right font-semibold">{`$${service.price}`}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </CardContent>
-        </Card>
+                </TableHeader>
+                <TableBody>
+                  {nails_service_list[current_lang as keyof LanguageServices][
+                    service_key
+                  ].map((service) => (
+                    <TableRow key={service.name}>
+                      <TableCell>{service.name}</TableCell>
+                      <TableCell className="text-right font-semibold">{`$${service.price}`}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        ))}
       </section>
     </div>
   );
