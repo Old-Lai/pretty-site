4327f05784c8db91eba473c07c77ee3945b5f7e3
Henry <89488963+Old-Lai@users.noreply.github.com>
2025-03-19 17:20:48 -0700

    working service links

---
+++ src/components/navbar/FooterNav.tsx
@@ -0,0 +1,57 @@
+import logo_small from "@/assets/logo-small.svg";
+import { navOptions } from "./navOptions";
+import { Link } from "react-router-dom";
+
+export default function FooterNav() {
+  return (
+    <div className="container">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+        <div className="space-y-4 lg:text-lg">
+          <img
+            src={logo_small}
+            alt="Pretty Beauty Salon"
+            className="h-15 w-auto lg:h-[9rem]"
+          />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            at purus hendrerit, posuere quam tincidunt.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h3 className="font-semibold lg:text-xl">Services</h3>
+          <ul className="text-muted-foreground space-y-2 text-sm lg:text-lg">
+            {navOptions.map((option, index) => {
+              if (index === 0) return;
+              return (
+                <li key={option.name}>
+                  <Link to={option.to}>{option.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="space-y-4">
+          <h3 className="font-semibold lg:text-xl">Hours</h3>
+          <ul className="text-muted-foreground space-y-2 text-sm lg:text-lg">
+            <li>Monday - Friday: 9am - 8pm</li>
+            <li>Saturday: 9am - 6pm</li>
+            <li>Sunday: 10am - 5pm</li>
+          </ul>
+        </div>
+        <div className="space-y-4">
+          <h3 className="font-semibold lg:text-xl">Contact</h3>
+          <ul className="text-muted-foreground space-y-2 text-sm lg:text-lg">
+            <li>1735 Ocean Ave, San Francisco, CA 94112</li>
+            <li>info@prettysalon.com</li>
+          </ul>
+        </div>
+      </div>
+      <div className="text-muted-foreground mt-8 border-t pt-8 text-center text-sm">
+        <p>
+          &copy; {new Date().getFullYear()} Pretty Beauty Salon. All rights
+          reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
